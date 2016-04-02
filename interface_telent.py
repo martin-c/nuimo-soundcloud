dbafce423df75c084bb334d10cc53e843617c129
martin <martin.clemons@mac.com>
2016-04-02 19:44:01 +0200

    Create basic telent interface for VLC.

--- interface_telent.py
+++ interface_telent.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import, unicode_literals
 
 import re
 import telnetlib
+#import Image, ImageFont, ImageDraw
+
 
 
 class NuimoEvent(object):
@@ -32,6 +34,7 @@ class NuimoEvent(object):
         :return:
         """
         import re
+        # http://www.regexr.com/3d4tq
         verb, value = re.match('([BRS])\,([LRUD]?[-?\d]*)', self.raw_event).groups()
         return verb, value
 
@@ -54,6 +57,20 @@ class NuimoEvent(object):
         return self.raw_event
 
 
+class NuimoGlyphs(object):
+    """
+    A class consisting of Nuimo graphics objects.
+    """
+    def __init__(self):
+        self.char_cache = {}
+        super(NuimoGlyphs, self).__init__()
+
+    def get_bitmap(self, char):
+        if char in char_cache:
+            return char_cache[char]
+
+
+
 class MediaPlayerController(object):
     """
     A high-level interface to the media player
@@ -72,14 +89,34 @@ class MediaPlayerController(object):
         """
         pass
 
-    def increase_volume(self):
+    def pause(self):
+        """
+        Pause media playback.
+        :return:
+        """
+
+    def skip_forward(self):
+        """
+        Skip forward in media playback.
+        :return:
+        """
+        pass
+
+    def skip_backward(self):
+        """
+        Skip backward in media playback.
+        :return:
+        """
+        pass
+
+    def increase_volume(self, value):
         """
         Increase media player volume.
         :return:
         """
         pass
 
-    def decrease_volume(self):
+    def decrease_volume(self, value):
         """
         Decrease media player volume.
         :return:
@@ -92,12 +129,93 @@ class TelnetVlcController(MediaPlayerController):
     A controller for VLC media player over Telent interface
     """
     def __init__(self):
-        self.connection = telnetlib.Telnet(localhost, 4212)
+        self.connection = telnetlib.Telnet('localhost', 4212)
+        self.connection.read_until('Password: ')
+        self.connection.write(b'secret\n')
+        print self.connection.read_until('>')
         super(TelnetVlcController, self).__init__()
 
+    def play(self):
+        print self.connection.write(b'play\n')
+
+    def pause(self):
+        print self.connection.write(b'pause\n')
+
+    def stop(self):
+        print self.connection.write(b'stop\n')
+
+    def skip_forward(self):
+        print self.connection.write(b'next\n')
+
+    def skip_backward(self):
+        print self.connection.write(b'prev\n')
+
+    def increase_volume(self, value):
+        s = b'volup {:d}\n'.format(value)
+        print self.connection.write(s)
+
+    def decrease_volume(self, value):
+        s = b'voldown {:d}\n'.format(value)
+        print self.connection.write(s)
 
 
+class NuimoController(object):
+    """
+    A stateful media controller object.
+    """
+    def __init__(self, player_interface):
+        self.player_interface = player_interface
+        self.nuimo_states = {
+            'button_pressed': False
+        }
+        self.player_states = {
+            'playing': False
+        }
+        super(NuimoController, self).__init__()
 
+    def _button_press(self):
+        """
+        Nuimo button has been pressed
+        :return:
+        """
+        print('button press', 'player state: ', self.player_states['playing'])
+        if self.player_states['playing'] is False:
+            self.player_interface.play()
+            self.player_states['playing'] = True
+        else:
+            self.player_interface.pause()
+            self.player_states['playing'] = False
+
+    def process_event(self, event):
+        """
+        Process an event from the Nuimo controller
+        :param event:
+        :return:
+        """
+        if event.verb == 'B':
+            if event.value == '1':
+                self.nuimo_states['button_pressed'] = True
+            else:
+                if self.nuimo_states['button_pressed'] is True:
+                    self._button_press()
+                self.nuimo_states['button_pressed'] = False
+
+        elif event.verb == 'R':
+            if event.value > 0:
+                self.player_interface.increase_volume(min(1, int(int(event.value) / 16.0)))
+            else:
+                self.player_interface.decrease_volume(min(1, int(int(event.value) / -16.0)))
+
+        elif event.verb == 'S':
+            if event.value == 'R':
+                self.player_interface.skip_forward()
+                self.player_states['playing'] = True
+            elif event.value == 'L':
+                self.player_interface.skip_backward()
+                self.player_states['playing'] = True
+            elif event.value == 'D':
+                self.player_interface.stop()
+                self.player_states['playing'] = False
 
 
 def run_interface():
@@ -108,12 +226,15 @@ def run_interface():
     from websocket import create_connection
 
     ws = create_connection('ws://localhost:8086/')
+    nuimo = NuimoController(TelnetVlcController())
 
     try:
         while True:
             event = NuimoEvent(ws.recv())
             print "Received '%s'" % event
             print(event.verb, event.value)
+            nuimo.process_event(event)
+
     except KeyboardInterrupt:
         pass
 
